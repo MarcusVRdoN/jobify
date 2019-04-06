a6ac78143d99702840526f67059f34e1a9272fdf
MarcusVRdoN <marcusvrdon@gmail.com>
2019-04-06 20:46:04 -0300

    Blocking admin for non localhost.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ var port = process.env.PORT || 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use("/admin", function(request, response, next) {
+  if(request.hostname === "localhost") {
+    next();
+  }
+  else {
+    response.send("Not allowed!");
+  }
+});
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extend: true }));
 
